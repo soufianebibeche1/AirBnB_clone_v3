738741f71dbca4f7940eb4586ef6e467d194340f
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-02-25 15:45:41 +0000

    add db_storage and file-storage functions and test task 2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,17 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Retrieve one object"""
+        if cls and id:
+            key = "{}.{}".format(cls.__name__, id)
+            return self.__objects.get(key)
+
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        if cls:
+            return len([obj for obj in self.__objects.values()
+                        if type(obj) == cls])
+        else:
+            return len(self.__objects)
